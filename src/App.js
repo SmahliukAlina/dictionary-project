4ffa988982a91ab7862dbc4d210d1e6dcb717961
Alina Smahliuk <88776329+SmahliukAlina@users.noreply.github.com>
2022-06-03 20:48:32 +0300

    Added container and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://classy-semolina-bd4086.netlify.app/"
-          target="_blank"
-          rel="noreferrer noopener"
-          title="Alina`s Portfolio Site"
-        >
-          Alina Smahliuk
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/SmahliukAlina/dictionary-project"
-          target="_blank"
-          rel="noreferrer noopener"
-          title="Project on GitHub"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header"></header>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://classy-semolina-bd4086.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Alina`s Portfolio Site"
+          >
+            Alina Smahliuk
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/SmahliukAlina/dictionary-project"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Project on GitHub"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
