1c6aa52569b7c1dd0a72c93b02245b8cf3ad94e6
Alina Smahliuk <88776329+SmahliukAlina@users.noreply.github.com>
2022-06-03 20:45:25 +0300

    footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://classy-semolina-bd4086.netlify.app/"
+          target="_blank"
+          rel="noreferrer noopener"
+          title="Alina`s Portfolio Site"
+        >
+          Alina Smahliuk
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/SmahliukAlina/dictionary-project"
+          target="_blank"
+          rel="noreferrer noopener"
+          title="Project on GitHub"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
