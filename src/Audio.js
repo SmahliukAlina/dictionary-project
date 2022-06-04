f720585ead7293c93c4559cf5d502273f8264947
Alina Smahliuk <88776329+SmahliukAlina@users.noreply.github.com>
2022-06-04 16:41:50 +0300

    CSS updated

--- src/Audio.js
+++ src/Audio.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./Audio.css";
 
 export default function Audio(props) {
   if (props.data.audio) {
     return (
-      <a href={props.data.audio} target="_blank" rel="noreferrer noopener">
-        <i className="fa-solid fa-volume-high"></i>
-      </a>
+      <span className="Audio">
+        <a href={props.data.audio} target="_blank" rel="noreferrer noopener">
+          <i className="fa-solid fa-volume-high"></i>
+        </a>
+      </span>
     );
   } else {
     return null;
