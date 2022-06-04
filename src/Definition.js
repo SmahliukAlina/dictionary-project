5c71b316e4037924c903c5e56388b065044cb28b
Alina Smahliuk <88776329+SmahliukAlina@users.noreply.github.com>
2022-06-04 17:18:13 +0300

    Added css to synonyms and example

--- src/Definition.js
+++ src/Definition.js
@@ -1,15 +1,21 @@
 import React from "react";
+import "./Definition.css";
 import Synonyms from "./Synonyms";
 
 export default function Definition(props) {
+  function showExample(item) {
+    if (item) {
+      return <span>Example: "{item}"</span>;
+    } else {
+      return null;
+    }
+  }
+
   return (
     <div className="Definition mt-2">
-      {"- "}
-      {props.data.definition}
-      <div className="mt-2 text-center">
-        <em>{props.data.example}</em>
-      </div>
-      <div>
+      <div className="definition">{props.data.definition}</div>
+      <div className="example">{showExample(props.data.example)}</div>
+      <div className="synonyms">
         <Synonyms data={props.data.synonyms} />
       </div>
     </div>
